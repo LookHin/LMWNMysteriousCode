fb6fe6239d2eb9fe311df31b06dfb9019347529c
Khwanchai Kaewyos <khwanchai@gmail.com>
2022-07-14 20:30:16 +0700

    decode-reverse
    decode-reverse

---
+++ decode-reverse.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+)
+
+func reverseString(str string) string {
+	byte_str := []rune(str)
+	for i, j := 0, len(byte_str)-1; i < j; i, j = i+1, j-1 {
+		byte_str[i], byte_str[j] = byte_str[j], byte_str[i]
+	}
+	return string(byte_str)
+}
+
+func main() {
+	secret := "aWFuZ25vVzpOQU06RU5JTDp0YTpzdTpuaW9K"
+
+	sd, _ := base64.StdEncoding.DecodeString(secret)
+	fmt.Println(reverseString(string(sd)))
+}
